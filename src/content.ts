b8b9f0f46d8628d93cb4ebfa649c5013ee991ab2
MaximeCle <maximeclement96@gmail.com>
2025-08-05 17:26:39 +0200

    Initial commit

---
+++ src/content.ts
@@ -0,0 +1,32 @@
+export const visionItems = [
+  {
+    title: "Économies de coûts",
+    content:
+      "La gestion de documents papier est coûteuse. De l'achat de papier et d'encre à l'entretien de l'espace de stockage et à la main-d'œuvre nécessaire pour gérer ces documents, les coûts s'additionnent rapidement.",
+  },
+  {
+    title: "Respect de l'environnement",
+    content:
+      "La dématérialisation des documents commerciaux réduit l'empreinte environnementale des entreprises. Moins de papier signifie moins d'arbres abattus et moins d'énergie dépensée pour l'impression.",
+  },
+  {
+    title: "Sécurité et conformité",
+    content:
+      "La dématérialisation permet de mettre en place des mesures de sécurité robustes, telles que le cryptage des données et l'accès restreint, garantissant la confidentialité et la conformité avec les réglementations en vigueur.",
+  },
+  {
+    title: "Accessibilité améliorée",
+    content:
+      "Les documents numériques peuvent être accessibles de n'importe où, à tout moment, ce qui favorise la mobilité et le travail à distance. Cette accessibilité améliorée permet aux entreprises de rester flexibles et de s'adapter plus facilement aux changements.",
+  },
+  {
+    title: "Réduction des erreurs",
+    content:
+      "La saisie manuelle de données peut entraîner des erreurs coûteuses. La dématérialisation permet d'automatiser la collecte et la transmission de données, réduisant ainsi le risque d'erreurs humaines.",
+  },
+  {
+    title: "Gain de place",
+    content:
+      "Les locaux de stockage de documents physiques peuvent être coûteux et encombrants. La dématérialisation permet de libérer de l'espace et d'optimiser l'utilisation des locaux de l'entreprise.",
+  },
+];
