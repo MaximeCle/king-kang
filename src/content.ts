f4ce3eb02d053ffef9cdf379487713bae8394604
MaximeCle <maximeclement96@gmail.com>
2025-08-11 18:32:02 +0200

    add section home page

--- src/content.ts
+++ src/content.ts
@@ -1,32 +1,205 @@
 export const visionItems = [
   {
+    icon: "wallet",
     title: "Économies de coûts",
     content:
       "La gestion de documents papier est coûteuse. De l'achat de papier et d'encre à l'entretien de l'espace de stockage et à la main-d'œuvre nécessaire pour gérer ces documents, les coûts s'additionnent rapidement.",
   },
   {
+    icon: "tree",
     title: "Respect de l'environnement",
     content:
       "La dématérialisation des documents commerciaux réduit l'empreinte environnementale des entreprises. Moins de papier signifie moins d'arbres abattus et moins d'énergie dépensée pour l'impression.",
   },
   {
+    icon: "shield",
     title: "Sécurité et conformité",
     content:
       "La dématérialisation permet de mettre en place des mesures de sécurité robustes, telles que le cryptage des données et l'accès restreint, garantissant la confidentialité et la conformité avec les réglementations en vigueur.",
   },
   {
+    icon: "view",
     title: "Accessibilité améliorée",
     content:
       "Les documents numériques peuvent être accessibles de n'importe où, à tout moment, ce qui favorise la mobilité et le travail à distance. Cette accessibilité améliorée permet aux entreprises de rester flexibles et de s'adapter plus facilement aux changements.",
   },
   {
+    icon: "check-ring",
     title: "Réduction des erreurs",
     content:
       "La saisie manuelle de données peut entraîner des erreurs coûteuses. La dématérialisation permet d'automatiser la collecte et la transmission de données, réduisant ainsi le risque d'erreurs humaines.",
   },
   {
+    icon: "box",
     title: "Gain de place",
     content:
       "Les locaux de stockage de documents physiques peuvent être coûteux et encombrants. La dématérialisation permet de libérer de l'espace et d'optimiser l'utilisation des locaux de l'entreprise.",
+  },
+];
+
+export const pricingItems = [
+  {
+    title: "Découverte",
+    price: 15,
+    description:
+      "Tincidunt diam, malesuada elementum justo. Ut neque nulla, pulvinar ac pulvinar non",
+    hightlight: false,
+    options: [
+      {
+        enabled: true,
+        option: "Configuration KingKang",
+      },
+      {
+        enabled: true,
+        option: "Devis",
+      },
+      {
+        enabled: true,
+        option: "Gestion des contacts",
+      },
+      {
+        enabled: true,
+        option: "Statistiques",
+      },
+      {
+        enabled: true,
+        option: "Coffre-fort numérique",
+      },
+      {
+        enabled: false,
+        option: "Signatures à valeur probatoire",
+      },
+      {
+        enabled: false,
+        option: "Multi-utilisateur",
+      },
+      {
+        enabled: false,
+        option: "Intégration du catalogue",
+      },
+    ],
+  },
+  {
+    title: "Bronze",
+    price: 50,
+    description:
+      "Tincidunt diam, malesuada elementum justo. Ut neque nulla, pulvinar ac pulvinar non",
+    hightlight: false,
+    options: [
+      {
+        enabled: true,
+        option: "Configuration KingKang",
+      },
+      {
+        enabled: true,
+        option: "Devis",
+      },
+      {
+        enabled: true,
+        option: "Gestion des contacts",
+      },
+      {
+        enabled: true,
+        option: "Statistiques",
+      },
+      {
+        enabled: true,
+        option: "Coffre-fort numérique",
+      },
+      {
+        enabled: false,
+        option: "Signatures à valeur probatoire",
+      },
+      {
+        enabled: false,
+        option: "Multi-utilisateur",
+      },
+      {
+        enabled: false,
+        option: "Intégration du catalogue",
+      },
+    ],
+  },
+  {
+    title: "Argent",
+    price: 100,
+    description:
+      "Tincidunt diam, malesuada elementum justo. Ut neque nulla, pulvinar ac pulvinar non",
+    hightlight: true,
+    options: [
+      {
+        enabled: true,
+        option: "Configuration KingKang",
+      },
+      {
+        enabled: true,
+        option: "Devis",
+      },
+      {
+        enabled: true,
+        option: "Gestion des contacts",
+      },
+      {
+        enabled: true,
+        option: "Statistiques",
+      },
+      {
+        enabled: true,
+        option: "Coffre-fort numérique",
+      },
+      {
+        enabled: true,
+        option: "Signatures à valeur probatoire",
+      },
+      {
+        enabled: true,
+        option: "Multi-utilisateur",
+      },
+      {
+        enabled: false,
+        option: "Intégration du catalogue",
+      },
+    ],
+  },
+  {
+    title: "Or",
+    price: 150,
+    description:
+      "Tincidunt diam, malesuada elementum justo. Ut neque nulla, pulvinar ac pulvinar non",
+    hightlight: false,
+    options: [
+      {
+        enabled: true,
+        option: "Configuration KingKang",
+      },
+      {
+        enabled: true,
+        option: "Devis",
+      },
+      {
+        enabled: true,
+        option: "Gestion des contacts",
+      },
+      {
+        enabled: true,
+        option: "Statistiques",
+      },
+      {
+        enabled: true,
+        option: "Coffre-fort numérique",
+      },
+      {
+        enabled: true,
+        option: "Signatures à valeur probatoire",
+      },
+      {
+        enabled: true,
+        option: "Multi-utilisateur",
+      },
+      {
+        enabled: true,
+        option: "Intégration du catalogue",
+      },
+    ],
   },
 ];
