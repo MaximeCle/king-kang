b21edada9c8a88b47f3aa1203abf237528b4c08c
MaximeCle <maximeclement96@gmail.com>
2025-09-04 16:59:58 +0200

    add page contact

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,7 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 
-// 2. Import loader(s)
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 
-// 3. Define your collection(s)
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
   schema: z.object({
@@ -15,5 +12,4 @@ const blog = defineCollection({
   }),
 });
 
-// 4. Export a single `collections` object to register your collection(s)
 export const collections = { blog };
